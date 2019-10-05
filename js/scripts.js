de702864a1cca89bddd11466771fc681718b2f33
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2019-10-04 19:56:47 -0700

    Update scripts.js
    removed uneeded curly bracer from end of  repository var

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 var repository = [
-    { name: "Bulbasaur", height: 2.4, types: ["grass", "poison"] },
-    { name: "Charizard", height: 5.7, types: ["fire", "flying"] },
-    { name: "Wigglytuff", height: 3.3, types: ["normal", "fairy"] }
-]
-};
+  { name: "Bulbasaur", height: 2.4, types: ["grass", "poison"] },
+  { name: "Charizard", height: 5.7, types: ["fire", "flying"] },
+  { name: "Wigglytuff", height: 3.3, types: ["normal", "fairy"] }
+];
