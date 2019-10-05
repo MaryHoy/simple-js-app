d83fb3d7e32f80d353a882a2d44a4f3c62e36c90
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2019-10-04 19:49:57 -0700

    Update scripts.js
    created variable repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert("Hello world");
-
-var favoriteFood = "broccoli";
-document.write(favoriteFood);
+var repository = [
+    { name: "Bulbasaur", height: 2.4, types: ["grass", "poison"] },
+    { name: "Charizard", height: 5.7, types: ["fire", "flying"] },
+    { name: "Wigglytuff", height: 3.3, types: ["normal", "fairy"] }
+]
+};
