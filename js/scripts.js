8b4f980fbd12e398ec208f5928cdd3df2057b9da
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2019-10-11 23:20:34 -0700

    Update scripts.js
    change for to foreach

--- js/scripts.js
+++ js/scripts.js
@@ -5,43 +5,39 @@ var repository = [
   { name: "Blastoise", height: 5.3, types: ["Water"] }
 ];
 
-function loopBlockFunction(currentItem) {
-  console.log(currentItem);
-}
-
-for (var i = 0; i < repository.length; i++) {
+repository.forEach(function(item) {
   var size;
-  if (repository[i].height > 3.9) {
+  if (item.height > 4.9) {
     size = "<b>Wow, that’s big!</b>";
   } else {
     size = "<i>It's a small Pokemon.</i>";
   }
 
   var result;
-  for (var iItem = 0; iItem < repository[i].types.length; iItem++) {
-    if (repository[i].types[iItem] == "Grass") {
+  item.types.forEach(function(itemType) {
+    if (itemType == "Grass") {
       result = '<span style="color:green;"> ';
-    } else if (repository[i].types[iItem] == "Fire") {
+    } else if (itemType == "Fire") {
       result = '<span style="color:red;"> ';
-    } else if (repository[i].types[iItem] == "Fairy") {
+    } else if (itemType == "Fairy") {
       result = '<span style="color:purple;"> ';
-    } else if (repository[i].types[iItem] == "Water") {
+    } else if (itemType == "Water") {
       result = '<span style="color:blue;"> ';
     }
-  }
-
+  });
   document.write(
     '<div class="box">' +
-      repository[i].name +
+      item.name +
       " (Height: " +
-      repository[i].height +
+      item.height +
       ")" +
       "<br>" +
       size +
       result +
       "<br>" +
-      repository[i].types +
+      item.types +
       "<p>" +
       "</div>"
   );
-}
+});
+console.log(pokemonRepository);
